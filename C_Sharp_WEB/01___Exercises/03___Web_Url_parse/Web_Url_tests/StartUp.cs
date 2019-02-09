2b7200d8db04dfc9ed48d15894ed70324b35c936
petko <ptpetkov666@gmail.com>
2019-02-09 22:55:35 +0200

    simple web url parse example

---
+++ C_Sharp_WEB/01___Exercises/03___Web_Url_parse/Web_Url_tests/StartUp.cs
@@ -0,0 +1,25 @@
+﻿namespace Web_Url_tests
+{
+    using System;
+    using System.Net;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var url = WebUtility.UrlEncode("https://www.softuni.come/?name=pesho");
+            string urlSecond = "https://softuni.bg:447/search?Query=pesho&users=true#go";
+            string decodedUrl = WebUtility.UrlDecode(urlSecond);
+
+            Uri parsedUri = new Uri(decodedUrl);
+            Console.WriteLine(parsedUri);
+
+            Console.WriteLine(parsedUri.Scheme);
+            Console.WriteLine(parsedUri.Host);
+            Console.WriteLine(parsedUri.Port);
+            Console.WriteLine(parsedUri.AbsolutePath);
+            Console.WriteLine(parsedUri.Query);
+            Console.WriteLine(parsedUri.Fragment);
+        }
+    }
+}
