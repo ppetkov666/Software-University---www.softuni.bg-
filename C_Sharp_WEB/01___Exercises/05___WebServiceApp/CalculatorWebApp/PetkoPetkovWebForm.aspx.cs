0773e86c624447c3a7bb25ac480c4362b21c9df8
Petko Petkov <ptpetkov666@gmail.com>
2019-02-11 17:45:36 +0200

    web service app

---
+++ C_Sharp_WEB/01___Exercises/05___WebServiceApp/CalculatorWebApp/PetkoPetkovWebForm.aspx.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace CalculatorWebApp
+{
+    public partial class PetkoPetkovWebForm : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnAdd_Click(object sender, EventArgs e)
+        {
+            // if we add or change our webservice we have to update Calculatorservice in order to access new methods 
+            // this has to be added into allowCookies="true"
+
+            // what we are doing here is we are accesing web service though a proxy interface as we create an instance of 
+            // this class and accessing his method ADD
+            //We access web service using HTTP protocol and messages are exchanged with SOAP format
+            //(SOAP is an XML-based protocol for accessing web services over HTTP)
+
+            //the proxy class will prepare soap request message, will call the method Add,  web service will execute it and it will return 
+            // Soap response back to the proxy class. the proxy will DE serialize the message and it will display it 
+            //The process of serialization   and DE serialization is done by the Proxy class
+
+            //visual studio generate proxy class using Web Service Description Language(WSDL) document of the web service
+            //WSDL contains the methods, params and return types 
+            CalculatorService.CalculatorWebServiceSoapClient client = new CalculatorService.CalculatorWebServiceSoapClient();
+            lblResult.Text = client.Add(int.Parse(txtFirstNumber.Text), int.Parse(txtSecondNumber.Text)).ToString();
+
+            gvCalculations.DataSource =  client.GetCalculations();
+            gvCalculations.DataBind();
+            gvCalculations.HeaderRow.Cells[0].Text = "Recent Calculations";
+        }
+    }
+}
---
+++ C_Sharp_WEB/01___Exercises/05___WebServiceApp/WebServiceApp/CalculatorWebService.asmx.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+
+namespace WebServiceApp
+{
+    /// <summary>
+    /// Summary description for CalculatorWebService
+    /// </summary>
+    [WebService(Namespace = "http://petkopetkov.org/webservices")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.None)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
+    // [System.Web.Script.Services.ScriptService]
+    public class CalculatorWebService : System.Web.Services.WebService
+    {
+        // webmethod attribute is mandatory if we want to use this method as a web service method
+        [WebMethod(EnableSession =true, Description ="this method add 2 numbers", CacheDuration =20)]
+        public int Add(int firstNumber, int secondNumber)
+        {
+            List<string> calculationResults;
+            if (Session["CALCULATIONRESULTS"] == null)
+            {
+                calculationResults = new List<string>();
+            }
+            else
+            {
+                calculationResults = (List<string>)Session["CALCULATIONRESULTS"];
+            }
+            string strRecentCalculations = firstNumber.ToString() + " + " + secondNumber.ToString() + " = "
+                 + (firstNumber + secondNumber).ToString();
+            calculationResults.Add(strRecentCalculations);
+            Session["CALCULATIONRESULTS"] = calculationResults;
+            return firstNumber + secondNumber;
+        }
+        // [WebServiceBinding(ConformsTo = WsiProfiles.None)]
+        [WebMethod(MessageName ="Add3Numbers")]
+        public int Add(int firstNumber, int secondNumber, int thirdNumber)
+        {
+            return firstNumber + secondNumber + thirdNumber;
+        }
+
+        [WebMethod(EnableSession =true)]
+        public List<string> GetCalculations()
+        {
+            if (Session["CALCULATIONRESULTS"] == null)
+            {
+                List<string> calculationresults = new List<string>();
+                calculationresults.Add("You have not performed any calculations");
+                return calculationresults;
+            }
+            else
+            {
+                return (List<string>)Session["CALCULATIONRESULTS"];
+            }
+        }
+    }
+}
