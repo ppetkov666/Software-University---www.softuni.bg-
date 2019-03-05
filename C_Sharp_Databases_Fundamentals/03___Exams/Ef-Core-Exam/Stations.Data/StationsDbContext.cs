8e2520d598ceac50717e61fdface6783e8fd583e
Petko Petkov <ptpetkov666@gmail.com>
2019-03-05 17:57:17 +0200

    Ef exam in progress

---
+++ C_Sharp_Databases_Fundamentals/03___Exams/Ef-Core-Exam/Stations.Data/StationsDbContext.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Stations.Data
+{
+	public class StationsDbContext : DbContext
+	{
+		public StationsDbContext()
+		{
+		}
+
+		public StationsDbContext(DbContextOptions options)
+			: base(options)
+		{
+		}
+
+		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+		{
+			if (!optionsBuilder.IsConfigured)
+			{
+				optionsBuilder.UseSqlServer(Configuration.ConnectionString);
+			}
+		}
+
+		protected override void OnModelCreating(ModelBuilder modelBuilder)
+		{
+		}
+	}
+}
---
+++ C_Sharp_Databases_Fundamentals/03___Exams/Ef-Core-Exam/Stations.Models/TrainSeat.cs
@@ -0,0 +1,27 @@
+﻿namespace Stations.Models
+{
+    using System.ComponentModel.DataAnnotations;
+    public class TrainSeat
+    {
+        public int Id { get; set; }
+
+        public int TrainId { get; set; }
+
+        [Required]
+        public Train Train { get; set; }
+
+        [Required]
+        public int SeatingClassId { get; set; }
+        public SeatingClass SeatingClass { get; set; }
+
+        [Required]
+        [Range(0, int.MaxValue)]
+        public int Quantity { get; set; }
+
+        // this is the other way if the value type is decimal - to be non negative value
+        //[Range(typeof(decimal), "0", "79228162514264337593543950335")]
+        //public decimal QuantityDecimal { get; set; }
+
+
+    }
+}
